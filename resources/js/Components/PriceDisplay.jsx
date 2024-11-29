5dc18e4dd5326d59ca455c1e8a358deabe3e1068
Salvo Bonanno <salvob.labs@gmail.com>
2024-11-29 05:53:38 +0100

    Removes buggy normalization

--- resources/js/Components/PriceDisplay.jsx
+++ resources/js/Components/PriceDisplay.jsx
@@ -1,7 +1,7 @@
 export default function PriceDisplay({value, currency}) {
     return (
         <span className="font-semibold text-gray-800 dark:text-gray-200">
-    {value.toFixed(2)} {currency}
+    {value} {currency}
   </span>
     );
 }
