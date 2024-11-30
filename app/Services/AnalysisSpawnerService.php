ffbd4949eb49bbb088bd61f5dd48cdde23a42875
Salvo Bonanno <salvob.labs@gmail.com>
2024-11-30 12:21:28 +0100

    Doubles max number of listing that can be fetched and adds testing for AnalysisSpawnerService

--- app/Services/AnalysisSpawnerService.php
+++ app/Services/AnalysisSpawnerService.php
@@ -41,11 +41,47 @@ class AnalysisSpawnerService
         $inventory_data = $this->discogsService()->fetchInventoryData($inventory->seller_username);
         $pagination_data = $inventory_data['pagination'];
         $pages = $pagination_data['pages'];
-        $inventory->update(['total_listings_count' => $pagination_data['items']]);
-        $this->analysis->update(['jobs' => $pages]);
-        $jobs = Collection::times($pages)
-            ->map(fn($page_number) => new FetchInventoryPageJob($inventory, $page_number))
-            ->toArray();
+        $listings_count = $pagination_data['items'];
+        $inventory->update(['total_listings_count' => $listings_count]);
+
+        // DISCOGS will block you to fetch over 100 pages for inventories, so getting 100 items per page
+        // you can list only 10k records
+        // but if you run two crawls ordering the records for the same field but inverting the order in the two
+        // batches you can fetch the double of the records
+
+        // Listing count is less or equal than 10k
+        if ($listings_count <= 10000) {
+            $this->analysis->update(['jobs' => $pages]);
+            $jobs = Collection::times($pages)
+                ->map(fn($page_number) => new FetchInventoryPageJob($inventory, $page_number))
+                ->toArray();
+        } else {
+
+            $half_listing_count = $listings_count / 2;
+            // Listing count is less or equal 20k (20k / 2 = 10k)
+            if ($half_listing_count <= 10000) {
+                $this->analysis->update(['jobs' => $pages]);
+                $desc_jobs = Collection::times($pages / 2)
+                    ->map(fn($page_number) => new FetchInventoryPageJob($inventory, $page_number, 'listed', 'desc'))
+                    ->toArray();
+                $asc_jobs = Collection::times($pages / 2)
+                    ->map(fn($page_number) => new FetchInventoryPageJob($inventory, $page_number, 'listed', 'asc'))
+                    ->toArray();
+
+            }
+            // Listing count is over 20k
+            else {
+                $this->analysis->update(['jobs' => 20000]);
+                $desc_jobs = Collection::times(100)
+                    ->map(fn($page_number) => new FetchInventoryPageJob($inventory, $page_number, 'listed', 'desc'))
+                    ->toArray();
+                $asc_jobs = Collection::times(100)
+                    ->map(fn($page_number) => new FetchInventoryPageJob($inventory, $page_number, 'listed', 'asc'))
+                    ->toArray();
+
+            }
+            $jobs = array_merge($desc_jobs, $asc_jobs);
+        }
 
         $batch = Bus::batch($jobs)
             ->before(function (Batch $batch) {
